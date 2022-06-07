09704ec3db075fb1af9ff22943cca6a5ee541ba6
PedroCosta138 <pedrocosta1111.pc@gmail.com>
2022-06-07 21:16:51 +0100

    inserir tabelas

--- app/src/main/java/pt/reviews/movies/TabelaBDActors.kt
+++ app/src/main/java/pt/reviews/movies/TabelaBDActors.kt
@@ -1,4 +1,21 @@
 package pt.reviews.movies
 
-class TabelaBDActors {
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaBDActors(db: SQLiteDatabase) : TabelaBD(db, NOME) {
+
+    override fun cria() {
+        db.execSQL(
+            "CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "$CAMPO_TITULO TEXT NOT NULL, $CAMPO_REALIZADOR TEXT NOT NULL )"
+        )
+    }
+
+    companion object {
+        const val NOME = "Actors"
+        const val CAMPO_NOME = "Nome"
+        const val CAMPO_REALIZADOR = "Realizador"
+
+    }
 }
--- app/src/main/java/pt/reviews/movies/TabelaBDMovies.kt
+++ app/src/main/java/pt/reviews/movies/TabelaBDMovies.kt
@@ -7,13 +7,15 @@ class TabelaBDMovies(db: SQLiteDatabase) : TabelaBD(db, NOME)  {
 
     override fun cria() {
         db.execSQL("CREATE TABLE $nome (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT, " +
-                "$CAMPO_TITULO TEXT NOT NULL, $CAMPO_REALIZADOR TEXT NOT NULL )")
+                "$CAMPO_TITULO TEXT NOT NULL, $CAMPO_REALIZADOR TEXT NOT NULL, $CAMPO_rDate TEXT NOT NULL,$CAMPO_DURACAO TEXT NOT NULL )")
     }
 
     companion object {
         const val NOME = "Movies"
         const val CAMPO_TITULO = "titulo"
         const val CAMPO_REALIZADOR = "Realizador"
+        const val CAMPO_DURACAO = "Duração"
+        const val CAMPO_rDate = "Data de estreia"
 
     }
 }
