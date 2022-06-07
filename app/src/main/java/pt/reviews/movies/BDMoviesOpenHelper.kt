aa8eb424a1c72a7f942abe1c4ef7e01fc80c9baa
PedroCosta138 <pedrocosta1111.pc@gmail.com>
2022-06-07 19:30:40 +0100

    commit exemplo

---
+++ app/src/main/java/pt/reviews/movies/BDMoviesOpenHelper.kt
@@ -0,0 +1,23 @@
+package pt.reviews.movies
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class BDMoviesOpenHelper(context: Context?) : SQLiteOpenHelper(context, NOME, null, VERSAO) {
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        requireNotNull(db)
+
+        TabelaBDActors(db).cria()
+        TabelaBDMovies(db).cria()
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+    }
+
+    companion object {
+        const val NOME = "livros.db"
+        private const val VERSAO = 1
+    }
+}
---
+++ app/src/main/java/pt/reviews/movies/TabelaBDActors.kt
@@ -0,0 +1,4 @@
+package pt.reviews.movies
+
+class TabelaBDActors {
+}
